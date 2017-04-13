51c50882d493433c6bccdd0b645b903b5a5a43fc
Harriet McKinnon <hsemckinnon@gmail.com>
2017-04-13 08:05:38 +0100

    add redux and react from cdns

---
+++ ReactReduxUI/src/reducers/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var redux_1 = require("redux");
+var rootReducer = redux_1.combineReducers({});
+exports.default = rootReducer;
+//# sourceMappingURL=index.js.map
---
+++ ReactReduxUI/src/store.ts
@@ -0,0 +1,15 @@
+﻿//import { createStore, applyMiddleware } from './libs/redux/redux.js';
+//import rootReducer from './reducers';
+//import * as thunk from './libs/redux/redux-thunk.js';
+
+//const logger = (store: any) => (next: any) => (action: any) => {
+//    console.group(action.type);
+//    console.info('dispatching', action);
+//    let result = next(action);
+//    console.log('next state', store.getState());
+//    console.groupEnd();
+
+//    return result;
+//};
+
+//export const createAppStore = () => createStore(rootReducer, applyMiddleware(thunk.default, logger));
